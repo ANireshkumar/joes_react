2fad65d03bdb4fddd3182f6779f69458bde3f00d
ANireshkumar <niresh1606@gmail.com>
2025-05-29 06:01:24 +0530

    Adding the css and use of bable in the react

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,13 +1,16 @@
-import LearnComponent from "./Components/LearnComponent"
+// import LearnComponent from "./Components/LearnComponent"
 
+import Header from "./Components/Header"
 
+import "./css/app.css"
 function App() {
  
 
   return (
     <>
-    <div>THIS IS PROJECT 1 learing the components</div>
-    <LearnComponent />
+    {/* <div>THIS IS PROJECT 1 learing the components</div>
+    <LearnComponent /> */}
+    <Header />
     </>
   )
 }
---
+++ project1/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+  return (
+    <div>
+        <h1 className="bannerText">This is the header</h1>
+        <p className="slogan">This is a paragraph</p>
+    </div>
+  )
+}
+
+export default Header
