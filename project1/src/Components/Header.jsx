cbc4fb2c7e6bdbafde793ffe6f1f521dcb652312
ANireshkumar <niresh1606@gmail.com>
2025-05-29 06:16:31 +0530

    Adding the dynamic and direct style in the react

--- project1/src/Components/Header.jsx
+++ project1/src/Components/Header.jsx
@@ -1,14 +1,18 @@
 
 const Header = () => {
+    let customcss = "blue";
   return (
     <div>
-       <h1>Js Expressinons</h1>
-<p>25+45={24+45}
-</p>
-<p>25-45={24-45}</p>
-<p>25*45={24*45}</p>
-<p>25/45={24/45}</p>
-<p>25%45={24%45}</p>
+       <h1>Adding the dynamic syling</h1>
+<p>This is the <span className={customcss}>dynamic styling</span></p>
+<p>This is the <span className={customcss}>dynamic styling</span></p>
+<p>This is the <span className={customcss}>dynamic styling</span></p>
+<p>This is the <span className="code">dynamic styling</span></p>
+<p>This is the <span className="blue">dynamic styling</span></p>
+
+<h1>This is a direct syling </h1>
+<p style={{color:"green "}}>This is the <span className="blue">dynamic styling</span></p>
+<p style={{color:"green , font-size:40px"}}>This is the <span className="blue">dynamic styling</span></p>
     </div>
   )
 }
